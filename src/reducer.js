428ca771af2fd1636d0a18f8ffc9e31225761e99
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-03-31 14:50:14 -0500

    cart complete

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,14 @@
 const reducer = (state, action) => {
-  if (action.type === 'CLEAR_CART') {
+  if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
-  if (action.type === 'REMOVE') {
+  if (action.type === "REMOVE") {
     return {
       ...state,
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
-  if (action.type === 'INCREASE') {
+  if (action.type === "INCREASE") {
     let tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload) {
         return { ...cartItem, amount: cartItem.amount + 1 };
@@ -18,18 +18,58 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
 
-  if (action.type === 'DECREASE') {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount - 1 };
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
       }
-      return cartItem;
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        if (action.payload.type === "inc") {
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        if (action.payload.type === "dec") {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+      }
+      return cartItem
     })
     .filter((cartItem) => cartItem.amount !== 0)
     return { ...state, cart: tempCart };
   }
-
-  return state;
+  throw new Error ('no matching action type')
 };
 
 export default reducer;
